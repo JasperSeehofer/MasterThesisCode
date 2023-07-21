a034c01b7d71fd6bdd2f7424449c7d53c292e530
Jasper <jasper@gulp-wwagner.de>
2023-07-21 09:33:00 +0200

    Test functions added

---
+++ master_thesis_code/main.py
@@ -0,0 +1,9 @@
+
+def main() -> None:
+    """
+    Run main to start the program.
+    """
+    print("I'm the main method.")
+
+if __name__ == "__main__":
+    main()
---
+++ master_thesis_code/test_module/test_func.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def miracle() -> None:
+    """
+    Print a mathematic miracle.
+    """
+    print("e^(i*pi) = -1")
