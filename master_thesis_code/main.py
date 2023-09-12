abbb13f2dca0a7aa472000456a7d4e36ab3105d8
Jasper <jasper@gulp-wwagner.de>
2023-09-12 15:06:31 +0200

    UNFINISHED: parameter estimation schwarzschild

--- master_thesis_code/main.py
+++ master_thesis_code/main.py
@@ -1,9 +1,19 @@
-from datamodels.parameter_space import ParameterSpace
+import logging
+
+from parameter_estimation.schwarzschild_eccentric_flux import SchwarzschildParameterEstimation
+
+# logging setup
+logging.basicConfig(filename='logfile.log', encoding='utf-8', level=logging.DEBUG)
 
 def main() -> None:
     """
     Run main to start the program.
     """
+    parameter_estimation = SchwarzschildParameterEstimation()
+
+    M_steps, M_differences_real, M_differences_imag = parameter_estimation.numeric_M_derivative()
+
+    parameter_estimation._plot_M_derivative(M_steps, M_differences_real, M_differences_imag)
     
 
 if __name__ == "__main__":
