36d6be56c0d809b71d67eaf2aabb516cfb4a3357
Jasper <jasper@gulp-wwagner.de>
2023-11-03 18:44:04 +0100

    Some files removed from repo and added to gitignore

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-xs = np.arange(-100, 100)
-ys = [1,2,3,4,5,6,7,8,9,0,134,2345,345,12]
-
-indices = np.round(np.linspace(0, len(xs) - 1, 10)).astype(int)
-
-print(xs[indices], ys[indices])
