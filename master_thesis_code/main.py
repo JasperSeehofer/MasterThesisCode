b015290ee85cda04969e786677e5b39ad8a781fe
Jasper Seehofer <jasper@gulp-wagner.de>
2023-12-07 00:07:24 +0100

    memory management added

--- master_thesis_code/main.py
+++ master_thesis_code/main.py
@@ -10,6 +10,7 @@ import GPUtil
 from tabulate import tabulate
 
 from master_thesis_code.arguments import Arguments
+from master_thesis_code.memory_management import MemoryManagement
 from master_thesis_code.parameter_estimation.parameter_estimation import ParameterEstimation, WaveGeneratorType
 from master_thesis_code.constants import SNR_THRESHOLD
 
@@ -27,27 +28,19 @@ def main() -> None:
     arguments.validate()
     _ROOT_LOGGER.info("---------- STARTING MASTER THESIS CODE ----------")
     
-    _ROOT_LOGGER.debug(f"CUDA version: 11.7")
-    _display_GPU_information()
-    mempool = cp.get_default_memory_pool()
-    fft_cache = cp.fft.config.get_plan_cache()
+    memory_management = MemoryManagement()
+    memory_management.display_GPU_information()
+    memory_management.display_fft_cache()
 
     parameter_estimation = ParameterEstimation(wave_generation_type=WaveGeneratorType.pn5, use_gpu=True)
-    check_dependency = False
-
-    if check_dependency:
-        for parameter_symbol in ["qS", "phiS"]:
-            parameter_estimation.check_parameter_dependency(parameter_symbol=parameter_symbol, steps=5)
 
     counter = 0
-    start_time = time()
-    GPU_usage = []
     while counter < arguments.simulation_steps:
-        GPU_usage.append(mempool.total_bytes()/10**9)
-        mempool.free_all_blocks()
-        GPU_usage.append(mempool.total_bytes()/10**9)
-        _ROOT_LOGGER.debug(f"currently used GPU memory: {int(mempool.used_bytes())/10**9}/{int(mempool.total_bytes())/10**9} Gbytes (used/total)")
-        _ROOT_LOGGER.info(f"{counter} evaluations successful. ({counter/(time()-start_time)*60}/min)")
+        memory_management.gpu_usage_stamp()
+        memory_management.memory_pool.free_all_blocks()
+        memory_management.gpu_usage_stamp()
+
+        _ROOT_LOGGER.info(f"{counter} evaluations successful. ({counter/(time()-memory_management._start_time)*60}/min)")
         parameter_estimation.parameter_space.randomize_parameters()
         snr = parameter_estimation.compute_signal_to_noise_ratio()
         if snr < SNR_THRESHOLD:
@@ -56,17 +49,16 @@ def main() -> None:
         else:
             _ROOT_LOGGER.info(f"SNR threshold check successful: {np.round(snr, 3)} >= {SNR_THRESHOLD}")
         cramer_rao_bounds = parameter_estimation.compute_Cramer_Rao_bounds()
-        _display_GPU_information()
-        fft_cache.show_info()
         parameter_estimation.save_cramer_rao_bound(cramer_rao_bound_dictionary=cramer_rao_bounds, snr=snr)
         counter += 1
 
-    plt.plot(GPU_usage)
-    plt.savefig("GPU_usage.png")
+        memory_management.display_GPU_information()
+        memory_management.display_fft_cache()
         
     parameter_estimation.lisa_configuration._visualize_lisa_configuration()
     parameter_estimation._visualize_cramer_rao_bounds()
     
+    memory_management.plot_GPU_usage()
     _ROOT_LOGGER.debug(f"Peak memory usage: {resource.getrusage(resource.RUSAGE_SELF).ru_maxrss *1e-6} in GB.")
 
 def _configure_logger(working_directory: str, log_level: int) -> None:
@@ -87,35 +79,6 @@ def _configure_logger(working_directory: str, log_level: int) -> None:
     plt.set_loglevel("warning")
 
     _ROOT_LOGGER.info(f"Log file location: {log_file_path}")
-
-def _display_GPU_information() -> None:
-    _ROOT_LOGGER.info("="*40, "GPU Details", "="*40)
-    gpus = GPUtil.getGPUs()
-    list_gpus = []
-    for gpu in gpus:
-        # get the GPU id
-        gpu_id = gpu.id
-        # name of GPU
-        gpu_name = gpu.name
-        # get % percentage of GPU usage of that GPU
-        gpu_load = f"{gpu.load*100}%"
-        # get free memory in MB format
-        gpu_free_memory = f"{gpu.memoryFree}MB"
-        # get used memory
-        gpu_used_memory = f"{gpu.memoryUsed}MB"
-        # get total memory
-        gpu_total_memory = f"{gpu.memoryTotal}MB"
-        # get GPU temperature in Celsius
-        gpu_temperature = f"{gpu.temperature} °C"
-        gpu_uuid = gpu.uuid
-        list_gpus.append((
-            gpu_id, gpu_name, gpu_load, gpu_free_memory, gpu_used_memory,
-            gpu_total_memory, gpu_temperature, gpu_uuid
-        ))
-
-
-    _ROOT_LOGGER.info(tabulate(list_gpus, headers=("id", "name", "load", "free memory", "used memory", "total memory",
-                                   "temperature", "uuid")))
 
 
 if __name__ == "__main__":
---
+++ master_thesis_code/memory_management.py
@@ -0,0 +1,86 @@
+import GPUtil
+from tabulate import tabulate
+import cupy as cp
+import logging
+import os
+import matplotlib.pyplot as plt
+from typing import List
+from time import time
+
+_LOGGER = logging.getLogger()
+
+class MemoryManagement:
+    memory_pool: cp.cuda.MemoryPool
+
+    def __init__(self):
+        self._gpu_monitor = GPUtil.getGPUS()
+        self.memory_pool = cp.get_default_memory_pool()
+        self._fft_cache = cp.fft.config.get_plan_cache()
+        self._start_time = time()
+        self._memory_pool_gpu_usage = []
+        self._gpu_usage = []
+        self._time_series = []
+
+            
+    def gpu_usage_stamp(self) -> None:
+        self._time_series.append(time() - self._start_time)
+        self.gpu_usage.append([gpu.memoryUsed/1000 for gpu in GPUtil.getGPUs()])
+        self.memory_pool_gpu_usage.append(int(self._memory_pool.total_bytes())/10**9)
+
+
+    def display_GPU_information(self) -> None:
+        _LOGGER.info("="*40, "GPU Details", "="*40)
+        gpus = GPUtil.getGPUs()
+        list_gpus = []
+        for gpu in gpus:
+            # get the GPU id
+            gpu_id = gpu.id
+            # name of GPU
+            gpu_name = gpu.name
+            # get % percentage of GPU usage of that GPU
+            gpu_load = f"{gpu.load*100}%"
+            # get free memory in MB format
+            gpu_free_memory = f"{gpu.memoryFree}MB"
+            # get used memory
+            gpu_used_memory = f"{gpu.memoryUsed}MB"
+            # get total memory
+            gpu_total_memory = f"{gpu.memoryTotal}MB"
+            # get GPU temperature in Celsius
+            gpu_temperature = f"{gpu.temperature} °C"
+            gpu_uuid = gpu.uuid
+            list_gpus.append((
+                gpu_id, gpu_name, gpu_load, gpu_free_memory, gpu_used_memory,
+                gpu_total_memory, gpu_temperature, gpu_uuid
+            ))
+
+        _LOGGER.info(tabulate(list_gpus, headers=("id", "name", "load", "free memory", "used memory", "total memory",
+                                   "temperature", "uuid")))
+   
+   
+    def display_fft_cache(self) -> None:
+        self._fft_cache.show_info()
+
+    def plot_GPU_usage(self) -> None:
+        figures_directory = f"saved_figures/monitoring/"
+
+        if not os.path.isdir(figures_directory):
+            os.makedirs(figures_directory)
+
+        fig = plt.figure(figsize=(12, 9))
+        plt.plot(
+            self._time_series,
+            self._memory_pool_gpu_usage,
+            "-",
+            label="Memory Pool",
+        )
+        for gpu_index in range(len(self._gpu_usage[0]) - 1):
+            plt.plot(
+            self._time_series,
+            self._gpu_usage[:][gpu_index],
+            "-",
+            label=f"GPU {gpu_index+1}",
+        )
+        plt.xlabel("t in sec")
+        plt.legend()
+        plt.savefig(figures_directory + "GPU_usage.png", dpi=300)
+        plt.clf()
