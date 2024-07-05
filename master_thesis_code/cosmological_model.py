ea5cc56289a5c95ad76971dd847e42ad11d1e688
hl306@uni-heidelberg.de <hd_hl306@uc2n994.localdomain>
2024-07-05 17:23:17 +0200

    new evaluation using mass for kde aswell and less precise measurements

--- master_thesis_code/cosmological_model.py
+++ master_thesis_code/cosmological_model.py
@@ -1860,7 +1860,7 @@ def use_detection(detection: Detection) -> bool:
     )
     distance_relative_error = detection.d_L_uncertainty / detection.d_L
 
-    if (distance_relative_error < 0.1) and (sky_localization_uncertainty < 0.0006):
+    if (distance_relative_error < 0.1) and (sky_localization_uncertainty < 0.001):
         return True
     _LOGGER.info(
         f"Detection skipped: distance_relative_error {distance_relative_error}, sky_localization_uncertainty {sky_localization_uncertainty}"
