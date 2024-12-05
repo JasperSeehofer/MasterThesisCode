8ca00f10706da8c4130a64507e1646fbe9cbf034
Jasper <jasper@gulp-wwagner.de>
2024-12-05 15:30:31 +0100

    added svg and latex annotation to plotter

--- master_thesis_code/bayesian_inference/scientific_plotter.py
+++ master_thesis_code/bayesian_inference/scientific_plotter.py
@@ -1,15 +1,22 @@
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import numpy as np
 from typing import Tuple
+
 
 class ScientificPlotter:
     figure: plt.Figure
     axis: plt.Axes
 
-    def __init__(self, figure_size: Tuple[int, int], rows: int = 1, columns: int = 1) -> None:
+    def __init__(
+        self, figure_size: Tuple[int, int], rows: int = 1, columns: int = 1
+    ) -> None:
         self.figure, self.axis = plt.subplots(rows, columns, figsize=figure_size)
+        plt.rcParams["text.usetex"] = True
 
-    def set_colormap_from_range(self, range: Tuple[float, float], cmap: str = 'viridis') -> None:
+    def set_colormap_from_range(
+        self, range: Tuple[float, float], cmap: str = "viridis"
+    ) -> None:
         norm = plt.Normalize(*range)
         self.color_map = cm.ScalarMappable(norm=norm, cmap=cmap)
 
@@ -27,3 +34,23 @@ class ScientificPlotter:
         self.axis.legend()
         plt.show()
         plt.close()
+
+    def save_as_svg(self, file_path: str, dpi: int) -> None:
+        self.axis.legend()
+        self.figure.savefig(file_path, format="svg", dpi=dpi)
+        plt.close()
+
+    def plot_example(self) -> None:
+        x = np.linspace(0, 5, 1000)
+        number_of_plots = 10
+        self.set_colormap_from_range((0, number_of_plots - 1))
+        for i in range(number_of_plots):
+            y = x * i
+            self.plot_colored(x, y, i, label=rf"${i} \cdot x$")
+        self.show_colorbar("Colorbar label")
+        self.save_as_svg(file_path="example.svg", dpi=300)
+
+
+if __name__ == "__main__":
+    plotter = ScientificPlotter((16, 9))
+    plotter.plot_example()
